ea913d367c9af070ceb8936f8a16c05e1c58c0d3
JAILSON COSTA GARRIDO <jailsongarrido23@gmail.com>
2024-04-25 21:30:16 -0300

    exercicio diferença

---
+++ exerciciosPython/ProjetoCadastrodeBatismo/main.py
@@ -0,0 +1,11 @@
+print("CADASTRO DE BATISMO")
+
+nomeBatizando = str (input("Digite o nome do batizando completo: "))
+nomeDosPais = str(input("Digite o nome dos pais completo: "))
+nomeDoPadreBatismo = str(input("Digite o nome do Padre: "))
+LoacalDoBatismo = str(input("Digite o Local do Batismo: "))
+anotacaoAdicionas = str(input("Digite algumas outras informações"))
+
+print(" SALVO...")
+print(nomeBatizando ,",",nomeDosPais,",",nomeDoPadreBatismo,",",LoacalDoBatismo)
+print(anotacaoAdicionas)
---
+++ exerciciosPython/exerciciodiferenca/main.py
@@ -0,0 +1,10 @@
+print("EXERCICIO DIFERENÇA")
+
+A = float(input())
+B = float(input())
+C = float(input())
+D = float(input())
+
+DIFERENCA = ((A * B) - (C*D))
+
+print(f"DIFERENÇA = {DIFERENCA}")
